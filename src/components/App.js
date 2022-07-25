b6eab275014770c9c55d654c6718901943bca764
Alex <friedmanalexg@gmail.com>
2022-07-25 11:04:37 -0400

    looking fetchy

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [pizzas, setPizzas] = useState ([]);
+
+
+  //fetch
+  useEffect(() => {
+    fetch("http://localhost:3001/pizzas")
+    .then(r => r.json())
+    .then(data => setPizzas([...pizzas, data]))
+
+
+  },[])
+
+console.log(pizzas)
   return (
     <>
       <Header />
